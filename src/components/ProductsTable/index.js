4acbddb69c32f34d97f700096ea532b922773afa
Amadriella <nconnect1789@gmail.com>
2023-01-16 16:40:33 +0100

    feat: display information of chosen product

--- src/components/ProductsTable/index.js
+++ src/components/ProductsTable/index.js
@@ -1,32 +1,97 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ProductsTable.css";
 
 const products = [
-  { id: 1, name: "t-shirt", year: 2012 },
-  { id: 2, name: "blouse", year: 2012 },
-  { id: 3, name: "trousers", year: 2016 },
-  { id: 4, name: "hat", year: 2011 },
-  { id: 5, name: "skirt", year: 2000 },
+  {
+    id: 1,
+    name: "t-shirt",
+    year: 2012,
+    color: "blue",
+    pantone_value: "11-2031",
+  },
+  {
+    id: 2,
+    name: "blouse",
+    year: 2012,
+    color: "blue",
+    pantone_value: "17-2031",
+  },
+  {
+    id: 3,
+    name: "trousers",
+    year: 2016,
+    color: "green",
+    pantone_value: "13-2031",
+  },
+  { id: 4, name: "hat", year: 2011, color: "red", pantone_value: "12-2031" },
+  {
+    id: 5,
+    name: "skirt",
+    year: 2000,
+    color: "yellow",
+    pantone_value: "15-2031",
+  },
+  {
+    id: 6,
+    name: "skirt",
+    year: 2000,
+    color: "yellow",
+    pantone_value: "15-2031",
+  },
 ];
 
-const ProductsArray = () => {
+const ProductsTable = (item) => {
+  const [productToShow, setProductToShow] = useState(null);
+
+  const handleOpenModal = (item) => {
+    setProductToShow(item);
+  };
+
+  const handleCloseModal = () => {
+    setProductToShow(null);
+  };
+
   return (
-    <div className="table-container">
-      <div className="row-wrapper">
-        <div className="table-title-cell-wrapper">Id</div>
-        <div className="table-title-cell-wrapper">Name</div>
-        <div className="table-title-cell-wrapper">Year</div>
-      </div>
-      {products.map((item, index) => {
-        return (
-          <div className="row-wrapper" key={index + item.name}>
-            <div className="table-cell-wrapper">{item.id}</div>
-            <div className="table-cell-wrapper">{item.name}</div>
-            <div className="table-cell-wrapper">{item.year}</div>
-          </div>
-        );
-      })}
-    </div>
+    <>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Products</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className="table-title-row">
+            <td className="table-title-cell"> Id</td>
+            <td className="table-title-cell"> Name</td>
+            <td className="table-title-cell"> Year</td>
+          </tr>
+          {products.map((item, index) => {
+            return (
+              <tr
+                className="table-row"
+                onClick={() => handleOpenModal(item)}
+                key={index + item.name}
+              >
+                <td className="table-cell">{item.id}</td>
+                <td className="table-cell">{item.name}</td>
+                <td className="table-cell">{item.year}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      {productToShow && (
+        <div className="product-container">
+          <p>Id: {productToShow.id} </p>
+
+          <p>Name: {productToShow.name}</p>
+          <p>Year: {productToShow.year}</p>
+          <p>Color: {productToShow.color}</p>
+          <p>Pantone value: {productToShow.pantone_value}</p>
+          <button onClick={handleCloseModal}>Close</button>
+        </div>
+      )}
+    </>
   );
 };
-export default ProductsArray;
+export default ProductsTable;
