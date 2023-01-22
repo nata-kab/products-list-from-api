c90ec8a64adeb9a81c471d5ef135c46e5de7d207
Amadriella <nconnect1789@gmail.com>
2023-01-22 13:45:30 +0100

    feat: FilterInput tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,12 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders the landing page", () => {
-  render(<App />);
+describe(`App`, () => {
+  it(`renders the landing page`, () => {
+    render(<App />);
 
-  expect(screen.getByRole("textbox")).toBeInTheDocument();
-  expect(screen.getByRole("table")).toBeInTheDocument();
-  expect(screen.getByRole("list")).toBeInTheDocument();
+    expect(screen.getByRole("textbox")).toBeInTheDocument();
+    expect(screen.getByRole("table")).toBeInTheDocument();
+    expect(screen.getByRole("list")).toBeInTheDocument();
+  });
 });
--- src/test-utils.tsx
+++ src/test-utils.tsx
@@ -5,13 +5,16 @@ import { Provider } from "react-redux";
 import apiDataReducer from "./store/slices/apiDataSlice";
 import apiFilterParamsReducer from "./store/slices/apiFilterParamsSlice";
 
-export const renderWithReduxState = (ui: ReactNode) => {
+export const renderWithReduxState = (ui: ReactNode, { actions = [] } = {}) => {
   const store = configureStore({
     reducer: {
       apiData: apiDataReducer,
       apiFilterParams: apiFilterParamsReducer,
     },
   });
+
+  actions.forEach((action) => store.dispatch(action));
+
   const renderResult = render(<Provider store={store}>{ui}</Provider>);
   return {
     ...renderResult,
